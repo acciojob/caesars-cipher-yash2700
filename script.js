3a7c4608958bd6a4a907d2096c34ad098a3ffb5f
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-03-16 13:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Your Script here.
+function a(str){
+    for(let i=0;i<str.length;i++){
+        let j=(String.fromCharCode(str[i].charCodeAt(0)+13))
+        let p1=str.substr(0,i);
+        let p2=str.substr(i+1);
+        str=p1+j+p2;
+    }
+    return (str)
+}
