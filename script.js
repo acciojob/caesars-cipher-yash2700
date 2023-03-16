44d2115a1b85d8bc3991dbdac21940739e17787a
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-03-16 13:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// Your Script here.
-function a(str){
-    for(let i=0;i<str.length;i++){
-        let j=(String.fromCharCode(str[i].charCodeAt(0)+13))
-        let p1=str.substr(0,i);
-        let p2=str.substr(i+1);
-        str=p1+j+p2;
-    }
-    return (str)
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
+
+function rot13(encodedStr){
+   const words = encodedStr.split(" ");  // String to Array
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+   for(let i=0; i< words.length; i++){
+        const word = words[i]
+        let decoded_word  = ''
+        for(let j = 0; j < word.length; j++) {
+            const char = word.charAt(j)
+            const decoded_char = lookup[char]
+            decoded_word += decoded_char
+        }
+        decodedArr.push(decoded_word)
+     }
+        return decodedArr.join(" ");
 }
